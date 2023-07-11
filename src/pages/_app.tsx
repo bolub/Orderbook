f5abb8442d84250d02616620d73ed68b37f37b0c
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-07-11 03:47:04 +0300

    Initial setup

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import { League_Spartan } from 'next/font/google';
+const leagueSpartan = League_Spartan({ subsets: ['latin'] });
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className={` ${leagueSpartan.className}`}>
+      <Component {...pageProps} />
+    </div>
+  );
 }
