73f9b663b661e4e259d762941d76e61f946115bf
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-07-11 04:49:50 +0300

    Create navbar

---
+++ src/components/UI/Navbar.tsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+  return (
+    <nav className='container mx-auto px-4 py-6 h-20 flex items-center'>
+      <p className='font-medium text-xl'>Orderbook</p>
+    </nav>
+  );
+};
