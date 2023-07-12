b845b1e485d7a44a296a08ecb13265dba7897ee0
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-07-12 23:25:35 +0300

    Fetch data when modal is open

--- src/components/TokenSelector/useTokenModal.tsx
+++ src/components/TokenSelector/useTokenModal.tsx
@@ -21,6 +21,7 @@ export const useTokenModal = () => {
     const { data: tokenList, isLoading } = useQuery({
       queryKey: ["tokensList"],
       queryFn: getTokensList,
+      enabled: isOpen,
     });
 
     const filteredTokenList = useMemo(() => {
