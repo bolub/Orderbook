996376b8eb6bb86084c9ceda964055685d7c0599
Boluwatife Oluwaseyi Abiola <abiol5202@gmail.com>
2023-07-13 05:04:24 +0300

    Update styles

--- src/components/TokenSelector/OrderBookTable.tsx
+++ src/components/TokenSelector/OrderBookTable.tsx
@@ -14,7 +14,7 @@ export const OrderBookTable = ({
   const sellers = parseMerchantData(data.asks.records);
 
   return (
-    <div className="mx-auto mb-10 min-h-[70vh] w-full">
+    <div className="mx-auto mb-10 min-h-[70vh] w-full max-w-[800px]">
       <button onClick={goBack} className="mb-4 flex items-center text-sm">
         <span className="mr-2">
           <HiOutlineChevronLeft />
@@ -22,30 +22,41 @@ export const OrderBookTable = ({
         Go Back
       </button>
 
-      <div className="h-full rounded-2xl border bg-white px-4 shadow-sm">
+      <div className="h-full rounded-2xl border bg-white shadow-sm">
         <div className="flex h-full w-full">
           {/* buyers */}
-          <div className="w-1/2 border-r pr-4 pt-6">
+          <div className="w-1/2 border-r pt-6">
             <table className="w-full table-auto">
               <thead>
                 <tr>
-                  <th className="text-left text-sm font-medium">Price(USD)</th>
-                  <th className="text-right text-sm font-medium">
+                  <th className="pl-4 text-left text-[13px] font-medium text-gray-600">
+                    Price(USD)
+                  </th>
+                  <th className="text-right text-[13px] font-medium text-gray-600">
                     Quantity(USD)
                   </th>
-                  <th className="text-right text-sm font-medium">Total(USD)</th>
+                  <th className="pr-4 text-right text-[13px] font-medium text-gray-600">
+                    Total(USD)
+                  </th>
                 </tr>
               </thead>
 
               <tbody>
                 {buyers.map((buyer, index) => {
                   return (
-                    <tr key={index}>
-                      <td className="py-2 text-sm">{buyer.price}</td>
+                    <tr
+                      key={index}
+                      className="cursor-pointer transition-colors hover:bg-gray-50"
+                    >
+                      <td className="py-2 pl-4 text-sm text-green-500">
+                        {buyer.price}
+                      </td>
                       <td className="py-2 text-right text-sm">
                         {buyer.quantity}
                       </td>
-                      <td className="py-2 text-right text-sm">{buyer.total}</td>
+                      <td className="py-2 pr-4 text-right text-sm">
+                        {buyer.total}
+                      </td>
                     </tr>
                   );
                 })}
@@ -54,28 +65,37 @@ export const OrderBookTable = ({
           </div>
 
           {/* sellers */}
-          <div className="w-1/2 pl-4 pt-6">
+          <div className="w-1/2 pt-6">
             <table className="w-full table-auto">
               <thead>
                 <tr>
-                  <th className="text-left text-sm font-medium">Price(USD)</th>
-                  <th className="text-right text-sm font-medium">
+                  <th className="pl-4 text-left text-[13px] font-medium text-gray-600">
+                    Total(USD)
+                  </th>
+                  <th className="text-left text-[13px] font-medium text-gray-600">
                     Quantity(USD)
                   </th>
-                  <th className="text-right text-sm font-medium">Total(USD)</th>
+                  <th className="pr-4 text-right text-[13px] font-medium text-gray-600">
+                    Price(USD)
+                  </th>
                 </tr>
               </thead>
 
               <tbody>
                 {sellers.map((seller, index) => {
                   return (
-                    <tr key={index}>
-                      <td className="py-2 text-sm">{seller.price}</td>
-                      <td className="py-2 text-right text-sm">
+                    <tr
+                      key={index}
+                      className="cursor-pointer transition-colors hover:bg-gray-50"
+                    >
+                      <td className="py-2 pl-4 text-left text-sm">
+                        {seller.total}
+                      </td>
+                      <td className="py-2 text-left text-sm">
                         {seller.quantity}
                       </td>
-                      <td className="py-2 text-right text-sm">
-                        {seller.total}
+                      <td className="py-2 pr-4 text-right text-sm text-red-500">
+                        {seller.price}
                       </td>
                     </tr>
                   );
